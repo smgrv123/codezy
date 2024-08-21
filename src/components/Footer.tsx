aa28263efc416cae5fd4337b48500f166699af1b
Sumrit Grover <sumritgrover1@gmail.com>
2024-08-21 22:31:03 +0530

    portfolio link added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,12 @@ export default function Footer() {
   return (
     <footer className='flex flex-row px-10 py-5 justify-between bg-inherit w-full border-t border-border-light dark:border-border-dark'>
       <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-      <div className='flex items-center'>Build With ❤️ by Sumrit Grover</div>
+      <div className='flex items-center'>
+        Build With ❤️ by{' '}
+        <a href='https://www.sumritgrover.com' className='ml-1'>
+          Sumrit Grover
+        </a>
+      </div>
     </footer>
   );
 }
